675dabef7fb6338be6addeefd64f117c2a7661aa
Eric W. Anderson <ewa@users.noreply.github.com>
2023-11-04 22:41:48 -0600

    Works, but there's no sanity-checking on anything, no useful debugging output, etc.

--- GeneralURLHandler/GeneralURLHandlerApp.swift
+++ GeneralURLHandler/GeneralURLHandlerApp.swift
@@ -6,29 +6,48 @@
 //
 
 import SwiftUI
+import Foundation
 import OSLog
 
 extension Logger {
-    static let logger = Logger(subsystem: "GeneralURLHandler", category: "general")
+    static let main = Logger(subsystem: "GeneralURLHandler", category: "general")
 }
 
 class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Set preference if needed.
+        let defaults = UserDefaults.standard
+        if defaults.string(forKey: "cmd") == nil {
+            defaults.set("/opt/homebrew/bin/emacsclient", forKey: "cmd")
+        }
+    }
+    
   func application(_: NSApplication, open urls: [URL]) {
-      Logger.logger.info("bob")
-    guard let conf = try? loadConfig(path: getConfigFilePath()) else { return }
 
-    guard let url = NSWorkspace.shared.urlForApplication(withBundleIdentifier: "org.alacritty")
-    else { return }
+    Logger.main.info("Starting: \(urls, privacy:.public)")
+    if (urls.count != 1) {
+          Logger.main.error("List too long or too short")
+          return
+    }
+    let cmd:String = UserDefaults.standard.string(forKey: "cmd")!
+    Logger.main.info("command preference: \(cmd,  privacy:.public)")
+    
+    let ec_url = NSURL(fileURLWithPath: cmd)
+      
     let configuration = NSWorkspace.OpenConfiguration()
-    configuration.createsNewApplicationInstance = true
-    print(urls)
-    guard let (kasten, zid) = urls.first.flatMap({ extractZettelLocation(url: $0) }) else { return }
+    configuration.createsNewApplicationInstance = false
+    configuration.promptsUserIfNeeded = true
+      configuration.activates = false
+      configuration.hides = true
+    configuration.arguments = [cmd, urls[0].absoluteString]
+      Logger.main.info("Command url: \(ec_url as URL, privacy:.public), command arguments: \(configuration.arguments,  privacy:.public)")
 
-    guard let zhome = conf.kasten[kasten] else { return }
-
-    let zettelAbs = "\(zhome)/docs/\(zid).md"
-
-    configuration.arguments = ["-e", conf.nvim, zettelAbs]
-    NSWorkspace.shared.openApplication(at: url, configuration: configuration, completionHandler: nil)
+    //NSWorkspace.shared.openApplication(at: ec_url as URL, configuration: configuration, completionHandler: nil)
+      do {
+          try Process.run(ec_url as URL, arguments: [urls[0].absoluteString])
+      }  catch {
+          return
+      }
   }
 }
