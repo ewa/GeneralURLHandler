a08d3246f918ec5e2b6512a7872e13c6a73d51c6
Eric W. Anderson <ewa@users.noreply.github.com>
2023-11-06 14:31:34 -0700

    A little more debugging output (which wasn't relevant to the problem I was trying to address) but otherwise no meaningful change

--- GeneralURLHandler/GeneralURLHandlerApp.swift
+++ GeneralURLHandler/GeneralURLHandlerApp.swift
@@ -10,7 +10,7 @@ import Foundation
 import OSLog
 
 extension Logger {
-    static let main = Logger(subsystem: "GeneralURLHandler", category: "general")
+    static let main = Logger(subsystem: "GeneralURLHandler", category: "main")
 }
 
 class AppDelegate: NSObject, NSApplicationDelegate {
@@ -23,31 +23,43 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         }
     }
     
-  func application(_: NSApplication, open urls: [URL]) {
-
-    Logger.main.info("Starting: \(urls, privacy:.public)")
-    if (urls.count != 1) {
-          Logger.main.error("List too long or too short")
-          return
+    func application(_: NSApplication, open urls: [URL]) {
+        
+        Logger.main.info("Starting: \(urls, privacy:.public)")
+        if (urls.count != 1) {
+            Logger.main.error("List too long or too short")
+            return
+        }
+        let cmd:String = UserDefaults.standard.string(forKey: "cmd")!
+        Logger.main.info("command preference: \(cmd,  privacy:.public)")
+        
+        let ec_url = NSURL(fileURLWithPath: cmd)
+        
+        let configuration = NSWorkspace.OpenConfiguration()
+        configuration.createsNewApplicationInstance = false
+        configuration.promptsUserIfNeeded = true
+        configuration.activates = false
+        configuration.hides = true
+        configuration.arguments = [cmd, urls[0].absoluteString]
+        Logger.main.info("Command url: \(ec_url as URL, privacy:.public), command arguments: \(configuration.arguments,  privacy:.public)")
+        
+        //NSWorkspace.shared.openApplication(at: ec_url as URL, configuration: configuration, completionHandler: nil)
+        let client:Process = Process()
+        client.executableURL = ec_url as URL
+        client.arguments = [urls[0].absoluteString]
+        let outPipe = Pipe()
+        let errPipe = Pipe()
+               
+        client.standardOutput=outPipe
+        client.standardError=errPipe
+        client.terminationHandler = { (process) in
+            Logger.main.debug("\nFinished. running: \(process.isRunning, privacy:.public), status: \(process.terminationStatus, privacy:.public)")
+        }
+        do {
+            try client.run()
+        } catch {
+            Logger.main.error("Well, shit")
+            return
+        }
     }
-    let cmd:String = UserDefaults.standard.string(forKey: "cmd")!
-    Logger.main.info("command preference: \(cmd,  privacy:.public)")
-    
-    let ec_url = NSURL(fileURLWithPath: cmd)
-      
-    let configuration = NSWorkspace.OpenConfiguration()
-    configuration.createsNewApplicationInstance = false
-    configuration.promptsUserIfNeeded = true
-      configuration.activates = false
-      configuration.hides = true
-    configuration.arguments = [cmd, urls[0].absoluteString]
-      Logger.main.info("Command url: \(ec_url as URL, privacy:.public), command arguments: \(configuration.arguments,  privacy:.public)")
-
-    //NSWorkspace.shared.openApplication(at: ec_url as URL, configuration: configuration, completionHandler: nil)
-      do {
-          try Process.run(ec_url as URL, arguments: [urls[0].absoluteString])
-      }  catch {
-          return
-      }
-  }
 }
